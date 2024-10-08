013879ca645154856f296dd206d8bf32726683e5
Tushaar <tushaar.sharma05@gmail.com>
2024-10-09 01:18:42 +0530

    added events.py

--- backend/app/events.py
+++ backend/app/events.py
@@ -1,17 +1,22 @@
 from llama_index.core.workflow import Event
-from pydantic import BaseModel, Field
-from typing import List, Optional
+from pydantic import Field
+from typing import List
 from .schema import *
 
 
 class ScraperEvent(Event):
     queries: List[str]
 
-
 class ResearchAnalysisEvent(Event):
     context: str
     papers: List[Paper] = Field(..., description="List of scraped papers")
 
-
 class UserRequestEvent(Event):
-    request_type
+    request_type: str = Field(..., description="Type of request: 'problem_statements' or 'methodologies'")
+    initial_resutl: InitialAnalysisResult
+
+class ProblemStatementsEvent(Event):
+    problem_statements: List[str] = Field(..., description="Generated problem statements")
+
+class MethodologiesEvent(Event):
+    methodologies: List[str] = Field(..., description="Suggested methodologies")
