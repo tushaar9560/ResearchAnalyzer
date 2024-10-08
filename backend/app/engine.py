ee33e08e606a0f9b7de6f562a146516037669855
Tushaar <tushaar.sharma05@gmail.com>
2024-10-09 02:15:18 +0530

    update engine.py

--- backend/app/engine.py
+++ backend/app/engine.py
@@ -1,12 +1,20 @@
+import logging
 from llama_index.core.workflow import Workflow, step, Context, StartEvent, StopEvent
 from .events import *
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 from ..utils import create_llm
-from .helper import create_conversation, prepare_conversation
+from .helper import create_conversation, prepare_conversation, add_message_to_conversation
+from .schema import *
+from .constants import *
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class AnalysisWorkflow(Workflow):
     def __init__(self, memory: Conversation = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.memory = memory if memory else create_conversation()
+        self.llm = create_llm("")
 
     @step
     async def query_creator(self, ctx: Context, event: StartEvent) -> ScraperEvent:
@@ -20,9 +28,6 @@ class AnalysisWorkflow(Workflow):
     async def gap_analyzer(self, event: ResearchAnalysisEvent) -> InitialAnalysisResult:
         pass
 
-    @step
-    async def related_works(self, event: ResearchAnalysisEvent) -> InitialAnalysisResult:
-        pass
 
     @step
     async def problem_statement(self, event: UserRequestEvent) -> ProblemStatementsEvent:
@@ -32,9 +37,6 @@ class AnalysisWorkflow(Workflow):
     async def methodologies(self, event: UserRequestEvent) -> MethodologiesEvent:
         pass
 
-    # @step
-    # async def lone_wolf(self, event: LoneLLMEvent):
-    #     pass
 
     @step
     async def final_result_collector(self, initial_result: InitialAnalysisResult, problem_statements: Optional[ProblemStatementsEvent], methodologies: Optional[MethodologiesEvent]) -> StopEvent:
